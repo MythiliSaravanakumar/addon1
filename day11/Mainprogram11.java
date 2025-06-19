fb6dca4b8dea28ba7a441f700363720d07203f46
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-19 10:48:23 +0530

    message

---
+++ day11/Mainprogram11.java
@@ -0,0 +1,31 @@
+package addon_day11;
+
+
+
+	import java.sql.Connection;
+	import java.sql.DriverManager;
+	import java.sql.SQLException;
+	import java.sql.Statement;
+
+public class Mainprogram11 {
+	
+		
+
+			public static void delete(Statement stmt) throws SQLException {
+				String query="DELETE FROM student WHERE mark=70 ";
+				stmt.executeUpdate(query);
+				System.out.println("Query executed");
+			}
+
+			public static void main(String[] args) throws ClassNotFoundException, SQLException {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				String url="jdbc:mysql://localhost:3306/sample";
+				String username="root";
+				String password="";
+				Connection con=DriverManager.getConnection(url,username,password);
+				Statement stmt=con.createStatement();
+				delete(stmt);
+				
+			}
+		}
+
